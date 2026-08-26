784968aaf1986f931ea71ac87ba171e7c0b04af1
Ahmed Abdulahi Ahmed <ahmed.ahmed@bekk.no>
2026-08-26 13:46:12 +0200

    fixed typos and correct referencing of scalewayauth

--- api/common/podtypes/scaleway.go
+++ api/common/podtypes/scaleway.go
@@ -1,1 +1,23 @@
-git
+package podtypes
+
+// Scaleway
+//
+// Configuration for interacting with Scaleway
+// +kubebuilder:object:generate=true
+type Scaleway struct {
+	// Configuration for authenticating a Pod with Scaleway
+	//
+	//+kubebuilder:validation:Optional
+	Auth *ScalewayAuth `json:"auth,omitempty"`
+}
+
+// ScalewayAuth
+//
+// Configuration for authenticating a Pod with Scaleway
+type ScalewayAuth struct {
+	// Name of the Kubernetes Secret (in the application's namespace) containing the
+	// Scaleway API access key and secret key.
+	//
+	//+kubebuilder:validation:Required
+	SecretName string `json:"secretName"`
+}
